39ea965f71e8f39a8d60404df69180131ea4c444
RumpaPaul <rumpakucse14@gmail.com>
2021-07-26 12:59:20 +0600

    worked on showing experiances-info dynamically from local storage (using map)

--- src/components/experiences/Experiences.js
+++ src/components/experiences/Experiences.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './ExperiencesStyle.css';
 import SectionHeader from '../sharedComponents/sectionHeader/SectionHeader';
+import { experiences } from '../../data/Experiences';
 
 const Experiences = () => {
-
+    const [Experiences] = React.useState(experiences);
     const [header_data] = React.useState({
         p: "My Resume",
         h: 'Working Experience',
@@ -15,66 +16,30 @@ const Experiences = () => {
                     <SectionHeader header_data={header_data} />
                 </header>
                 <div class="timeline">
-                    <div class="timeline-item left wow slideInLeft" data-wow-delay="0.1s">
-                        <div class="timeline-text">
-                            <div class="timeline-date">2045 - 2050</div>
-                            <h2>Web Developer</h2>
-                            <h4>Soft Agency, San Francisco, CA</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="timeline-item right wow slideInRight" data-wow-delay="0.1s">
-                        <div class="timeline-text">
-                            <div class="timeline-date">2045 - 2050</div>
-                            <h2>Web Developer</h2>
-                            <h4>Soft Agency, San Francisco, CA</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="timeline-item left wow slideInLeft" data-wow-delay="0.1s">
-                        <div class="timeline-text">
-                            <div class="timeline-date">2045 - 2050</div>
-                            <h2>Web Developer</h2>
-                            <h4>Soft Agency, San Francisco, CA</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="timeline-item right wow slideInRight" data-wow-delay="0.1s">
-                        <div class="timeline-text">
-                            <div class="timeline-date">2045 - 2050</div>
-                            <h2>Web Developer</h2>
-                            <h4>Soft Agency, San Francisco, CA</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="timeline-item left wow slideInLeft" data-wow-delay="0.1s">
-                        <div class="timeline-text">
-                            <div class="timeline-date">2045 - 2050</div>
-                            <h2>Web Developer</h2>
-                            <h4>Soft Agency, San Francisco, CA</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="timeline-item right wow slideInRight" data-wow-delay="0.1s">
-                        <div class="timeline-text">
-                            <div class="timeline-date">2045 - 2050</div>
-                            <h2>Web Developer</h2>
-                            <h4>Soft Agency, San Francisco, CA</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
-                            </p>
-                        </div>
-                    </div>
+                    {Experiences.map((item) =>
+                        <>
+                            {
+                                item.id % 2 === 0 ?
+                                    <div class="timeline-item right wow slideInRight" data-wow-delay="0.1s">
+                                        <div class="timeline-text">
+                                            <div class="timeline-date">{item.time_line}</div>
+                                            <h2>{item.job_title}</h2>
+                                            <h4>{item.company_name}</h4>
+                                            <p>{item.description}</p>
+                                        </div>
+                                    </div>
+                                    :
+                                    <div class="timeline-item left wow slideInLeft" data-wow-delay="0.1s">
+                                        <div class="timeline-text">
+                                            <div class="timeline-date">{item.time_line}</div>
+                                            <h2>{item.job_title}</h2>
+                                            <h4>{item.company_name}</h4>
+                                            <p>{item.description}</p>
+                                        </div>
+                                    </div>
+                            }
+                        </>
+                    )}
                 </div>
             </div>
         </div>
---
+++ src/data/Experiences.js
@@ -0,0 +1,52 @@
+export const experiences=[
+    {
+        id:1,
+        time_line: '2016 - 2018',
+        job_title: "Web Developer",
+        company_name: "Soft Agency, San Francisco, CA",
+        description: "Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium."
+    },
+    {
+        id:2,
+        time_line: '2012 - 2014',
+        job_title: "Web Designer",
+        company_name: "Soft Agency, San Francisco, CA",
+        description: "Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium."
+    },
+    {
+        id:3,
+        time_line: '2015 - 2017',
+        job_title: "Web testing",
+        company_name: "Soft Agency, San Francisco, CA",
+        description: "Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium."
+    },
+    {
+        id:4,
+        time_line: '2013 - 2020',
+        job_title: "Front End Developer",
+        company_name: "Soft Agency, San Francisco, CA",
+        description: "Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium."
+    },
+    {
+        id:5,
+        time_line: '2019 - 2021',
+        job_title: "Front End Designer",
+        company_name: "Soft Agency, San Francisco, CA",
+        description: "Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium."
+    },
+    {
+        id:6,
+        time_line: '2015 - 2017',
+        job_title: "Back End Developer",
+        company_name: "Soft Agency, San Francisco, CA",
+        description: "Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium."
+    },
+    {
+        id:7,
+        time_line: '2045 - 2050',
+        job_title: "Database Designer",
+        company_name: "Soft Agency, San Francisco, CA",
+        description: "Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium."
+    },
+  
+]
