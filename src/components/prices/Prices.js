e4e428518f2ba453cc7ae0556fae0b7aac928f38
RumpaPaul <rumpakucse14@gmail.com>
2021-07-26 17:39:40 +0600

    Imported section-header component (in price-section)

--- src/components/prices/Prices.js
+++ src/components/prices/Prices.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './PricesStyle.css';
 import { prices } from '../../data/Prices';
 import PriceCard from './priceCard/PriceCard';
+import SectionHeader from '../sharedComponents/sectionHeader/SectionHeader';
 
 const Prices = () => {
     const [Prices] = React.useState(prices);
+    const [header_data] = React.useState({
+        p: "Pricing Plan",
+        h: 'Affordable Price',
+    });
     return (
         <div class="price" id="price">
             <div class="container">
-                <div class="section-header text-center wow zoomIn" data-wow-delay="0.1s">
-                    <p>Pricing Plan</p>
-                    <h2>Affordable Price</h2>
+                <div class="text-center wow zoomIn" data-wow-delay="0.1s">
+                    <SectionHeader header_data={header_data} />
                 </div>
                 <div class="row">
                     <div class="col-md-4 wow fadeInUp" data-wow-delay="0.5s">
