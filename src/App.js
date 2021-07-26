ddd3bfea338b9e77838c6f098e080963c44e3b15
RumpaPaul <rumpakucse14@gmail.com>
2021-07-26 11:48:07 +0600

    Imported services section (HTML, CSS code)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Hero from "./components/hero/Hero";
 import NavBar from "./components/navBar/NavBar";
 import About from "./components/about/About";
+import Services from "./components/services/Services";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <NavBar />
             <Hero />
             <About />
+            <Services />
         </>
     );
 }
