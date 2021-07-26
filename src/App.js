b3562220c0cdbe5d4583c9bacd6f073f32c46739
RumpaPaul <rumpakucse14@gmail.com>
2021-07-26 12:27:22 +0600

    Imported Experiences section (HTML, CSS)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Hero from "./components/hero/Hero";
 import NavBar from "./components/navBar/NavBar";
 import About from "./components/about/About";
 import Services from "./components/services/Services";
+import Experiences from "./components/experiences/Experiences";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Hero />
             <About />
             <Services />
+            <Experiences />
         </>
     );
 }
