9dbad82b1af6ce87bf7796f95f1fb4de73b77519
RumpaPaul <rumpakucse14@gmail.com>
2021-07-27 11:05:15 +0600

    Imported Contact-section (HTML, CSS)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/about/About";
 import Services from "./components/services/Services";
 import Experiences from "./components/experiences/Experiences";
 import Prices from "./components/prices/Prices";
+import Contact from "./components/contacts/Contact";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
             <Services />
             <Experiences />
             <Prices />
+            <Contact />
         </>
     );
 }
